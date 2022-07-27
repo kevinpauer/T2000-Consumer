11f9a7768068d34bd6a433e51306f7a2eda4d0f1
Kevin Pauer <kevinpauer@t-online.de>
2022-07-27 12:01:04 +0200

    Add KafkaRecord support; Add Result topic

--- src/main/java/org/acme/consumer/PayloadConsumer.java
+++ src/main/java/org/acme/consumer/PayloadConsumer.java
@@ -1,47 +1,87 @@
 package org.acme.consumer;
 
 import io.quarkus.runtime.StartupEvent;
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
+import io.smallrye.reactive.messaging.kafka.KafkaRecord;
+import io.smallrye.reactive.messaging.kafka.Record;
 import java.sql.Timestamp;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 import org.acme.dto.NumberPayload;
+import org.acme.dto.Result;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
 import org.jboss.logging.Logger;
 
 public class PayloadConsumer {
 
   @Inject
   Logger logger;
+
+  @Inject
+  @Channel("result-topic")
+  Emitter<Result> resultEmitter;
+
   private Timestamp creationTimestamp;
-  private Timestamp terminationTimestemp;
   private Random random = new Random();
+  private Double[] resultArray = new Double[50000];
+
+  private static boolean isSorted(Double[] a) {
+    if (a == null) {
+      return false;
+    } else if (a.length == 0) {
+      return true;
+    }
+    for (int i = 0; i < a.length - 1; i++) {
+      if (a[i] > a[i + 1]) {
+        return false;
+      }
+    }
+    return true;
+  }
 
   public void onStart(@Observes final StartupEvent ev) {
     Date date = new Date();
     creationTimestamp = new Timestamp(date.getTime());
   }
 
-
   @Incoming("numbers-payload")
-  public void newPayload(NumberPayload payload) {
-    Double[] numbersArray = payload.getNumbersList();
+  public CompletionStage<Void> newPayload(KafkaRecord<Integer, NumberPayload> payload) {
+    Double[] numbersArray = payload.getPayload().getNumbersList();
+    logger.info(payload.getTimestamp());
     Date date = new Date();
     logger.info("Start arithmetic operation at: " + new Timestamp(date.getTime()));
-    doArithmeticOperation(numbersArray);
+    resultArray = doArithmeticOperation(numbersArray);
     date = new Date();
     logger.info("End arithmetic operation at: " + new Timestamp(date.getTime()));
+
+    sendResultToChannel();
+
+    return CompletableFuture.runAsync(()->{});
   }
 
-  private void doArithmeticOperation(Double[] numbersArray) {
+  private Double[] doArithmeticOperation(Double[] numbersArray) {
     for (int i = 0; i < numbersArray.length; i++) {
       for (int j = 0; j < 750; j++) {
         numbersArray[i] = numbersArray[i] + 0 + random.nextDouble() * 1000000;
       }
     }
     Arrays.sort(numbersArray);
+    return numbersArray;
+  }
+
+  private void sendResultToChannel() {
+    Date date = new Date();
+    Result result = new Result(new Timestamp(date.getTime()), isSorted(resultArray));
+    resultEmitter.send(result);
   }
 }
 
