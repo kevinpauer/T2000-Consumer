84bcfc031ba13be68e24e9f0ab1c78bf016a0a72
Kevin Pauer <kevinpauer@t-online.de>
2022-07-28 14:33:00 +0200

    Fix Endpoints

--- src/main/java/org/acme/dto/NumberPayload.java
+++ src/main/java/org/acme/dto/NumberPayload.java
@@ -1,6 +1,5 @@
 package org.acme.dto;
 
-import java.util.Date;
 import java.sql.Timestamp;
 import java.util.List;
 import lombok.Getter;
@@ -13,11 +12,4 @@ public class NumberPayload {
   Timestamp timestamp;
   List<Double> numbersList;
   Integer id;
-
-  public NumberPayload(Integer id, List<Double> numberList) {
-    Date date = new Date();
-    this.id = id;
-    this.numbersList = numberList;
-    this.timestamp = new Timestamp(date.getTime());
-  }
 }
