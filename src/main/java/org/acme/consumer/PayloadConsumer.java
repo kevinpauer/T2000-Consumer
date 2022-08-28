9d386b02def91f75f1c1cc740399f9df5c08101a
Kevin Pauer <kevinpauer@t-online.de>
2022-08-28 21:47:23 +0200

    Version 1.0, docker-compose not working with hosting consumers

--- src/main/java/org/acme/consumer/PayloadConsumer.java
+++ src/main/java/org/acme/consumer/PayloadConsumer.java
@@ -3,11 +3,9 @@ package org.acme.consumer;
 import io.quarkus.runtime.StartupEvent;
 import io.smallrye.reactive.messaging.kafka.KafkaRecord;
 import java.sql.Timestamp;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.stream.Collectors;
 import javax.enterprise.event.Observes;
@@ -15,6 +13,7 @@ import javax.inject.Inject;
 import org.acme.dto.NumberPayload;
 import org.acme.dto.Result;
 import org.acme.dto.ResultPayload;
+import org.eclipse.microprofile.config.ConfigProvider;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
@@ -22,13 +21,13 @@ import org.jboss.logging.Logger;
 
 public class PayloadConsumer {
 
+  public static final String CONSUMER_GROUP =
+      ConfigProvider.getConfig().getValue("consumer-group", String.class);
   @Inject
   Logger logger;
-
   @Inject
   @Channel("result-topic")
   Emitter<ResultPayload> resultEmitter;
-
   private Timestamp creationTimestamp;
   private Random random = new Random();
   private List<Double> resultArray;
@@ -55,47 +54,32 @@ public class PayloadConsumer {
   @Incoming("numbers-payload")
   public CompletionStage<Void> newPayload(KafkaRecord<Integer, NumberPayload> payload) {
     try {
-      logger.info("payload: " + payload);
-      logger.info("payload: " + payload.getPayload());
+      logger.info("Received numberPayload: " + payload);
       List<Double> numbersArray = payload.getPayload().getNumbersList();
-      logger.info(payload.getTimestamp());
-      Date date = new Date();
-      logger.info("Start arithmetic operation at: " + new Timestamp(date.getTime()));
       resultArray = doArithmeticOperation(numbersArray);
-      date = new Date();
-      logger.info("End arithmetic operation at: " + new Timestamp(date.getTime()));
       sendResultToChannel(payload.getPayload());
     } catch (Exception ex) {
       logger.warn("newPayload failed!");
     }
-
     return payload.ack();
   }
 
   private List<Double> doArithmeticOperation(List<Double> numbersArray) {
     for (int i = 0; i < numbersArray.size(); i++) {
-      for (int j = 0; j < 750; j++) {
-        numbersArray.set(i, numbersArray.get(i) * -1);
+      for (int j = 0; j < 200; j++) {
+        numbersArray.set(i, numbersArray.get(i) + Math.sqrt(numbersArray.get(i)) * 0.1);
       }
     }
     return numbersArray.stream().sorted().collect(Collectors.toList());
   }
 
   private void sendResultToChannel(NumberPayload incomingPayloadData) {
+    boolean isSorted = isSorted(resultArray);
     Date date = new Date();
-    Result result = new Result(1, new Timestamp(date.getTime()), isSorted(resultArray));
-    ResultPayload resultPayload = new ResultPayload(incomingPayloadData.getId(), incomingPayloadData.getNumbersList(), incomingPayloadData.getTimestamp(), result);
+    Result result = new Result(CONSUMER_GROUP, new Timestamp(date.getTime()),
+        isSorted);
+    ResultPayload resultPayload = new ResultPayload(incomingPayloadData.getId(),
+        incomingPayloadData.getNumbersList(), incomingPayloadData.getTimestamp(), result);
     resultEmitter.send(resultPayload);
   }
 }
-
-// Ergebnis topic erstellen mit den Ergebnissen der Rechenoperation
-// Array itterieren
-// - Sorting mit großer Liste
-// - Output in Ergebnis topic
-//
-// Kafka schauen wann Event erstellt wurde mit Metadaten
-// Kafka Rekord wegen Timestemp
-//
-// Experiment wenn setup, wie reagieren einzelne deployments auf verschiedene available ressource größen
-// Mehr ram etc
